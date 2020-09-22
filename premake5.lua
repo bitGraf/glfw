5d6b654a8a4574c7bcedf9522b19101ea3e1dd05
Andrew B <ablaskov27@gmail.com>
2020-09-22 12:00:10 -0500

    fix file includes

--- premake5.lua
+++ premake5.lua
@@ -10,12 +10,12 @@ project "GLFW"
         "include/GLFW/glfw3.h",
         "include/GLFW/glfw3native.h",
         "src/glfw_config.h",
-        "src/context.h",
-        "src/init.h",
-        "src/input.h",
-        "src/monitor.h",
-        "src/vulkan.h",
-        "src/window.h",
+        "src/context.c",
+        "src/init.c",
+        "src/input.c",
+        "src/monitor.c",
+        "src/vulkan.c",
+        "src/window.c",
     }
 
     defines
